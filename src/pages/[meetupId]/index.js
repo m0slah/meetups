6ae81cb267b32e67d1102b3d1cd256a330e2c932
m0slah <muhammedslahqadir@gmail.com>
2023-02-28 00:38:38 +0300

    SSG data fetching

--- src/pages/[meetupId]/index.js
+++ src/pages/[meetupId]/index.js
@@ -11,4 +11,42 @@ const meetupDetails = () => {
   );
 };
 
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+    fallback: false,
+  };
+}
+
+
+export async function getStaticProps(context) {
+  //fetching data from single meetup
+
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetup: {
+        image:
+          "https://images.pexels.com/photos/532263/pexels-photo-532263.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        id: meetupId,
+        title: "First Meetup",
+        address: "Some Street 5, Some City",
+        description: "The Meetup Description",
+      },
+    },
+  };
+}
+
 export default meetupDetails;
